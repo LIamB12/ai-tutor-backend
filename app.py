6ad2e9438a751f5300dbe2bf6c55125aa247215b
LIamB12 <liam.beenken@gmail.com>
2023-05-07 02:29:13 -0400

    gpt prompt

--- app.py
+++ app.py
@@ -18,11 +18,27 @@ def answer_question(input_string):
     inputs = input_string.split("_")
     userQuestion = inputs[0]
     userGrade = inputs[1]
+    tutor_prompt = "As a " + userGrade + " tutor, please help me solve the following homework question: " + userQuestion + '''
+
+    To solve this question, we can follow these steps:
+
+    1. Analyze the given problem and identify the key information.
+    2. Break down the problem into smaller steps or sub-questions.
+    3. Apply the relevant concepts or formulas to solve each step.
+    4. Show all the calculations and provide clear explanations along the way.
+    5. Double-check the solution for accuracy and make sure it aligns with the original problem.
+
+    Based on the given homework question, let's work through the solution together:
+
+    [Provide guidance, step-by-step explanations, and calculations based on the specific homework question]
+
+    Please let me know if you have any questions or need further clarification. I'm here to help you understand and solve the problem effectively.'''
+    
     response = co.generate(
     model='command-xlarge-nightly',
     prompt= "#context respond to the following prompt as an tutor for students in " + userGrade + ". If the prompt is not academics related, remind them to stay on topic. Here is the question: #prompt " + userQuestion + ".",
     max_tokens=300,
-    temperature=0.9,
+    temperature=0.1,
     k=0,
     stop_sequences=[],
     return_likelihoods='NONE')
@@ -36,7 +52,7 @@ def say_hi(input_string):
     model='command-xlarge-nightly',
     prompt= "Say hi to" + input_string + " and briefly introduce yourself as an online AI tutor ",
     max_tokens=300,
-    temperature=0.9,
+    temperature=0.1,
     k=0,
     stop_sequences=[],
     return_likelihoods='NONE')
